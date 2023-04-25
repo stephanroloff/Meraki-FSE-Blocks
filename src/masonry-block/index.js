b3f31b2e917c444621d87b61be38b6a18c378f24
Stephan Roloff <sr@werbeagenten.de>
2023-04-25 14:05:59 +0200

    masonry gallery block

--- src/masonry-block/index.js
+++ src/masonry-block/index.js
@@ -2,9 +2,11 @@ import { registerBlockType } from '@wordpress/blocks';
 import './style.scss';
 import Edit from './edit';
 import metadata from './block.json';
+import Icon from './Icon.js';
 
 
 registerBlockType(metadata, {
 	edit: Edit,
+	icon: Icon,
 	save: () => null
 });
--- src/masonry-block/render.php
+++ src/masonry-block/render.php
@@ -11,11 +11,61 @@
  */
 
 $wrapper_attributes = get_block_wrapper_attributes();
+//ACF
+$galleries = get_field('images');    
+
+$counter = 0;
+$overlayHover = $attributes['overlayHover']; 
+//convert 
+$amountSelected_number = (int)$attributes['amountSelected']; 
+
+if(!$galleries){
+    $galleries=[];
+}
 
 ?>
 <div <?php echo $wrapper_attributes?>>
-    <p>THIS A DYNAMIC BLOCK!</p>
-    <p>Attribute: <?php echo $attributes['amountSelected']?></p>
+    <main class="masonry-grid">
+
+            <?php  
+            foreach($galleries as $image){
+            $counter++;
+
+            if($amountSelected_number > 0){
+               if($counter > $amountSelected_number){
+                  break;
+               }
+            }
+            ?>
+
+            <div class="masonry-item">
+                  <div class="masonry-content">
+                     <div class="masonry-image">
+                        <img  src="<?php echo $image['image']['url'];?>" alt="">
+                        <div class="overlay" <?php echo $overlayHover? '':'style="display:none;"'?>>
+                           <div class="overlay-group">
+                              <div>
+                                 <h2><?php echo $image['name'];?></h2>
+                                 <p><?php echo $image['description'];?></p>
+                              </div>
+                              <?php if($image['link']!==''){ ?>
+                                 <p><a href="<?php echo $image['link'];?>">Mehr erfahren...</a></p>
+                              <?php } ?>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="spacer" style="height: 15px"></div>
+                  </div>
+            </div>
+
+            <?php
+            }
+            ?>
+
+         <?php
+         wp_reset_postdata(); 
+         ?>
+         </main>
 </div>
     
 
